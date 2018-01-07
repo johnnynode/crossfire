6524afca4d1f7cc6c9cc898741469f255b91e12d
johnnynode <johnnynode@gmail.com>
2018-01-07 21:04:53 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var connect = require('gulp-connect');
 var proxy = require('http-proxy-middleware');
 var plumber = require('gulp-plumber');
 var process = require('process');
-var sh = require('shelljs');
 var runSequence = require('run-sequence');
 var watch = require('gulp-watch');
 var imagemin = require('gulp-imagemin'); // 压缩image
@@ -13,6 +12,7 @@ var htmlmin = require('gulp-htmlmin'); // 压缩html
 var uglify = require('gulp-uglify'); // 压缩js
 var gutil = require('gulp-util');
 var del = require('del'); // 清空文件和文件夹
+var open = require('gulp-open');
 
 var allPath = {
     src: ['./src'],
@@ -24,6 +24,14 @@ var portFlag = 0; // 用于控制端口不同
 
 // 生产模式任务
 var productionTask = [];
+
+// 判断操作系统
+var platform = process.platform;
+
+// 定义一组browser的判断
+var browser = platform === 'linux' ? 'google-chrome' : (
+    platform === 'darwin' ? 'google chrome' : (
+        platform === 'win32' ? 'chrome' : 'firefox'));
 
 // clean task
 gulp.task('clean', function() {
@@ -64,20 +72,25 @@ gulp.task('watch', function() {
         .pipe(connect.reload());
 });
 
-//运行Gulp时,搭建起跨域服务器 开发模式下
-gulp.task('server', ['connect'], function() {
-    sh.echo("服务器开启!");
-    connectFlag = 0;
-    portFlag = 0;
-    sh.echo('将要打开浏览器访问：http://localhost:9012');
-    // avoid page crashed error transiently
-    var t = setTimeout(function() {
-        runSequence(['watch', 'open-browser']);
-        t = null;
-    }, 1500);
+// 打开浏览器的任务
+gulp.task('open', function() {
+    // gulp-open 的选项
+    var browserOptions = {
+        uri: 'http://localhost:' + (portFlag ? '8012' : '9012'),
+        app: browser
+    };
+    gulp.src(allPath.src)
+        .pipe(open(browserOptions));
 });
 
-// 开始构建
+//运行Gulp时,搭建起跨域服务器 开发模式下
+gulp.task('server', ['connect'], function() {
+    connectFlag = 0;
+    portFlag = 0;
+    runSequence(['watch', 'open']);
+});
+
+// 开始构建 todo
 gulp.task('build', ['clean'], function() {
     gutil.log(gutil.colors.yellow('构建开始!'));
     runSequence(productionTask, function() {
@@ -86,13 +99,7 @@ gulp.task('build', ['clean'], function() {
 });
 
 gulp.task('build-server', ['connect'], function() {
-    sh.echo("开始测试构建, 服务器开启!");
     connectFlag = 1;
     portFlag = 1;
-    sh.echo('将要打开浏览器访问：http://localhost:8012');
-    // avoid page crashed error transiently
-    var t = setTimeout(function() {
-        runSequence(['open-browser']);
-        t = null;
-    }, 1500);
+    runSequence(['open']);
 });
